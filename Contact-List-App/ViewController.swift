ab53789d1d66c6037e180822ae84d91533457442
mahmoud-atreesios <136765078+mahmoud-atreesios@users.noreply.github.com>
2024-07-30 03:28:40 +0300

    intial commit

---
+++ Contact-List-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Contact-List-App
+//
+//  Created by Mahmoud Mohamed Atrees on 30/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
